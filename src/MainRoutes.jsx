add0f95b73f0586ed79e2f9a2eab070eef574306
noizziday <danya.doulbekov@gmail.com>
2022-11-28 18:11:09 +0600

    dasdasdad

--- src/MainRoutes.jsx
+++ src/MainRoutes.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import AddPage from "./pages/AddPage";
+import EditPage from "./pages/EditPage";
+import RegistrationPage from "./pages/RegistrationPage";
+import LoginPage from "./pages/LoginPage";
 const MainRoutes = () => {
   return (
     <Routes>
