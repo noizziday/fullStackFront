0f40b951b1c81f3d732d0ea24fa51c85cbbdb8a8
Пользователь <dj31425364@gmail.com>
2022-11-29 00:17:08 +0600

    register login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import "@fontsource/raleway"
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return <div>    
+  <div
+style={{
+  width: "100vw",
+  height: "100vh",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: '#002939'
+}}
+>
+<div className="card" style={{height:'12vw', width: "100%", backgroundColor: 'white' }}>
+  <div className="card-body"  style={{ display: 'flex', justifyContent: 'center'}}>
+    <div style={{margin: '20px'}}>
+    <h2  style={{ width: '10vw', fontFamily: 'Raleway', fontWeight: '400', fontSize: '34px', border: 'solid', margin: '2px',}}>Sign in your account</h2>
+    <div className="mb-3">
+      <input
+        type="last_name"
+        className="form-control"
+        placeholder="*Username"
+        style={{ width: '10vw', fontFamily: 'Raleway', fontWeight: '400', fontSize: '18px' , border: 'solid', margin: '2px'}}
+      />
+    </div>
+    <div className="mb-3">
+      <input
+        type="username"
+        className="form-control"
+        placeholder="*Password"
+        style={{ width: '10vw', fontFamily: 'Raleway', fontWeight: '400', fontSize: '18px' , border: 'solid', margin: '2px'}}
+      />
+    </div>
+
+    <button
+      // onClick={handleClick}
+      className="btn btn-light"
+      style={{ width: '10vw', fontFamily: 'Raleway', fontWeight: '400', fontSize: '18px' , border: 'solid', margin: '2px'}}
+    >
+      Sign In
+    </button>
+    </div>
+  </div>
+</div>
+</div></div>;
 };
 
 export default LoginPage;
