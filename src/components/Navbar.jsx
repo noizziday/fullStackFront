a3920b044914059072723c1f03a37b1f0c805468
noizziday <danya.doulbekov@gmail.com>
2022-11-29 21:55:15 +0600

    for pull

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "../styles/Navbar.css";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <div className="navBlock">
       <div className="navContainer">
         <div className="navElements">
-          <div className="navElementsLinks">Главная</div>
+          <div className="navElementsLinks" onClick={() => navigate("/")}>
+            Главная
+          </div>
           <div className="navElementsLinks">Отели</div>
           <div className="navElementsLinks">Регистрация</div>
           <div className="navElementsLinks">Авторизация</div>
