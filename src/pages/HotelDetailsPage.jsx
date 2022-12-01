d0d49144e8cd96c76d1285902c3530ee1d532955
Aizhamal <aizhamal.batyrbekova@alatoo.edu.kg>
2022-12-02 01:26:54 +0600

    created details Page and added responsivness

---
+++ src/pages/HotelDetailsPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HotelDetails from "../components/HotelDetails";
+
+const HotelsDetailPage = () => {
+  return (
+    <>
+      <HotelDetails />
+    </>
+  );
+};
+
+export default HotelsDetailPage;
