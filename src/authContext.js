49413ac04fc26d9cd2833f5e8585b43044ea20b2
noizziday <danya.doulbekov@gmail.com>
2022-12-05 19:15:21 +0600

    добавил дропдавн меню для карточки

--- src/authContext.js
+++ src/authContext.js
@@ -1,100 +1,101 @@
-import React, { useState, useContext  } from 'react';
-import axios from 'axios';
+import React, { useState, useContext } from "react";
+import axios from "axios";
 
 export const authContext = React.createContext();
-export const useAuth = () => useContext(authContext)
+export const useAuth = () => useContext(authContext);
 
-const API = 'http://34.96.250.19';
+const API = "http://34.159.95.125";
 
 const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const [currentUser, setCurrentUser] = useState(false);
-    const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(false);
+  async function handleRegister(formData) {
+    setLoading(true);
+    try {
+      const res = await axios.post(`${API}/account/register/`, formData);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+      setError(Object.values(err.response.data).flat(2));
+    } finally {
+      setLoading(false);
+    }
+  }
 
+  async function handleLogin(formData, logInpValue, navigate, currentUser) {
+    setLoading(true);
+    try {
+      const res = await axios.post(`${API}/account/login/`, formData);
+      console.log(res);
+      localStorage.setItem("tokens", JSON.stringify(res.data));
+      localStorage.setItem("email", logInpValue);
+      setCurrentUser(logInpValue);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+      alert("Inputs are incorrect!");
+      setError([err.response.data.detail]);
+    } finally {
+      setLoading(false);
+    }
+  }
 
-    async function handleRegister(formData){
-        setLoading(true);
-        try {
-            const res = await axios.post(`${API}/account/register/`, formData);
-            console.log(res);
-        } catch(err) {
-            console.log(err);
-            setError(Object.values(err.response.data).flat(2));
-        } finally {
-            setLoading(false);
-        };
-    };
+  async function checkAuth() {
+    console.log("Check Auth Worked!");
+    setLoading(true);
+    try {
+      const tokens = JSON.parse(localStorage.getItem("tokens"));
+      const Authorization = `Bearer ${tokens.access}`;
+      const config = {
+        headers: {
+          Authorization,
+        },
+      };
+      const res = await axios.post(
+        `${API}/account/token/refresh/`,
+        { refresh: tokens.refresh },
+        config
+      );
+      localStorage.setItem(
+        "tokens",
+        JSON.stringify({
+          access: res.data.access,
+          refresh: tokens.refresh,
+        })
+      );
+      const email = localStorage.getItem("email");
+      setCurrentUser(email);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+      handleLogout();
+    } finally {
+      setLoading(false);
+    }
+  }
 
-    async function handleLogin(formData, logInpValue, navigate, currentUser){
-        setLoading(true);
-        try {
-            const res = await axios.post(`${API}/account/login/`, formData);
-            console.log(res);
-            localStorage.setItem('tokens', JSON.stringify(res.data));
-            localStorage.setItem('email', logInpValue);
-            setCurrentUser(logInpValue);
-            navigate('/');
-        } catch(err) {
-            console.log(err);
-            alert('Inputs are incorrect!')
-            setError([err.response.data.detail]);
-        } finally {
-            setLoading(false);
-        };
-    };
+  function handleLogout() {
+    localStorage.removeItem("tokens");
+    localStorage.removeItem("email");
+    setCurrentUser(false);
+  }
 
-    async function checkAuth() {
-        console.log('Check Auth Worked!');
-        setLoading(true);
-        try {
-            const tokens = JSON.parse(localStorage.getItem('tokens'));
-            const Authorization = `Bearer ${tokens.access}`;
-            const config = {
-                headers: {
-                    Authorization
-                }
-            };
-            const res = await axios.post(
-                `${API}/account/token/refresh/`,
-                { refresh: tokens.refresh },
-                config
-            );
-            localStorage.setItem('tokens', JSON.stringify({
-                access: res.data.access,
-                refresh: tokens.refresh
-            }));
-            const email = localStorage.getItem('email');
-            setCurrentUser(email);
-            console.log(res);
-        } catch(err) {
-            console.log(err);
-            handleLogout();
-        } finally {
-            setLoading(false);
-        };
-    };
-
-    function handleLogout() {
-        localStorage.removeItem('tokens');
-        localStorage.removeItem('email');
-        setCurrentUser(false);
-    };
-
-    return (
-        <authContext.Provider value={{
-            currentUser,
-            error,
-            handleRegister,
-            setError,
-            handleLogin,
-            checkAuth,
-            handleLogout
-
-        }}>
-            { children }
-        </authContext.Provider>
-    );
+  return (
+    <authContext.Provider
+      value={{
+        currentUser,
+        error,
+        handleRegister,
+        setError,
+        handleLogin,
+        checkAuth,
+        handleLogout,
+      }}>
+      {children}
+    </authContext.Provider>
+  );
 };
 
 export default AuthContextProvider;
